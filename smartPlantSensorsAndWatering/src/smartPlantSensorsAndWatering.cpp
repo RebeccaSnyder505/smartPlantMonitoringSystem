09d6d816c1b36557a233ea4610a479849b9a5945
R S <rebeccasnyder505@gmail.com>
2023-11-08 20:20:56 -0700

    all sensors and adafruit dash working, adafruit button triggers relay. Needs work:OLED

--- smartPlantSensorsAndWatering/src/smartPlantSensorsAndWatering.cpp
+++ smartPlantSensorsAndWatering/src/smartPlantSensorsAndWatering.cpp
@@ -51,7 +51,7 @@ Adafruit_MQTT_Publish feedAirPressure = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAM
 Adafruit_MQTT_Publish feedTempCelcius = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/smartflowerpot.tempcelcius");
 Adafruit_MQTT_Publish feedHumidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/smartflowerpot.humidity");
 Adafruit_MQTT_Publish feedPlantMoisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/smartflowerpot.plantmoisture");
-Adafruit_MQTT_Publish feedWaterButton = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/smartflowerpot.waterbutton");
+Adafruit_MQTT_Subscribe feedWaterButton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/smartflowerpot.waterbutton");
 
 /************Declare Variables*************/
 unsigned int last, lastTime;
@@ -80,7 +80,7 @@ int prevDustTime;
 const int timeIntervalAQ = 2000;
 const int timeIntervalDust = 5000;
 int prevAdafruitTime;
-const int timeIntervalAdafruit = 25000; 
+const int timeIntervalAdafruit = 40000; 
 const int MOISTPIN = A1;
 int moistureRead;
 const int timeIntervalMoisture = 5000;
@@ -91,6 +91,9 @@ const int timeIntervalBME280 = 5000;
 float tempC; 
 float pressPA;
 float humidRH;
+const int pumpPin = D9;
+const int moistureLow = 10; // ridiculously low, needs testing to find number
+bool givePlantWater;
 
 AirQualitySensor airquality(AQPIN); // declaring object airquality of class AirQualitySensor
 
@@ -122,6 +125,9 @@ void setup() {
   Serial.printf("\n\n"); 
   prevAdafruitTime = 0;
   pinMode(MOISTPIN, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  givePlantWater = false;
+  mqtt.subscribe(&feedWaterButton); 
 }
 
 
@@ -158,6 +164,10 @@ void loop() {
     prevMoistureTime = millis();
     moistureRead = analogRead(MOISTPIN);
     Serial.printf("moisture reading %d \n", moistureRead);  
+    if (moistureRead <= moistureLow) {
+      givePlantWater = true;
+      Serial.printf("moistureLow, give plant water = true \n");
+    }
   }
   if ((millis() - prevBME289Time) > timeIntervalBME280) { //read BME280 temp pres humid
     prevBME289Time = millis();
@@ -166,6 +176,37 @@ void loop() {
     humidRH = bme.readHumidity();  // in %RH
     Serial.printf("Temp %f Press %f Humid %f \n", tempC , pressPA , humidRH );
   }
+
+  //check button
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(100))) { // the subscription part
+    if (subscription == &feedWaterButton) {
+      Serial.printf("line 184, give plant water = true \n");
+      subValue = atof((char *)feedWaterButton.lastread);
+      Serial.printf("button pushed on adafrui");
+
+      givePlantWater = true;
+    }
+  }
+//   ///////begin pasting
+//       if( mqtt.Update() )
+//     {
+//         // this is our 'wait for incoming subscription packets' busy subloop
+//         // try to spend your time here
+//         Adafruit_MQTT_Subscribe *subscription;
+//         while ((subscription = mqtt.readSubscription(5000)))
+//         {
+//             if (subscription == &feedWaterButton)
+//             {
+//                 Serial.print(F("Got: "));
+//                 Serial.println((char *)feedWaterButton.lastread);
+//             }
+//         }
+//     }
+// ///end pasted code from adafruit-mqtt-spark-test.ino
+
+
+
   if ((millis()-prevAdafruitTime) > timeIntervalAdafruit) { //publish to Adafruit
     if(mqtt.Update()) {
       prevAdafruitTime = millis();
@@ -178,6 +219,15 @@ void loop() {
       feedTempCelcius.publish(tempC);
       feedPlantMoisture.publish(moistureRead);
     }
+  }
+  if (givePlantWater == true) {
+    givePlantWater = false;
+    digitalWrite(pumpPin, HIGH);
+    Serial.printf("relay closed \n");
+    delay (1000); // relay closed for very short time
+    digitalWrite(pumpPin, LOW);
+    Serial.printf("relay open \n");
+    delay(3000); // removoe after testing
   }
 }
 
@@ -223,3 +273,10 @@ void testdrawcircle(void) {
     display.display();
   }
 }
+
+
+
+
+
+
+
